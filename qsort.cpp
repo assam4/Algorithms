7d59fbcc87da03d6b01792731f06ff38648081f6
assam4 <115891697+assam4@users.noreply.github.com>
2024-03-15 01:20:29 +0400

    Add files via upload

---
+++ qsort.cpp
@@ -0,0 +1,48 @@
+#include <concepts>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+const int Unit = 1;
+const int Half = 2;
+
+template <typename T>  // limitation for correct operation
+concept Bidirectional_iterator = requires(T) 
+{ 
+    typename std::iterator_traits<T>::iterator_category;  
+};
+
+template <Bidirectional_iterator _iterator>
+void qsort(_iterator first, _iterator last )  
+{
+    auto left = first;
+    auto right = last ;
+    auto middle = std::next(left, std::distance(left, right) / Half);
+    
+    do
+    {
+        while (*left < *middle) ++left;
+        while (*right > *middle) --right;
+    
+        if (left <= right) 
+        {
+            std::iter_swap(left, right);
+            ++left;
+            --right;
+        }
+    }
+    while (left != right && left < right); 
+
+    if (right > first) qsort(first, right);
+    if (left < last) qsort(left, last);
+}
+
+int main()
+{
+    std::vector<int> vec = { 5 , 3 , 1 , 4, 2 , 6 , 9 , 10 , 7 , 8 };
+    static_assert(Bidirectional_iterator<decltype(vec.begin())>);  // check concept
+    qsort(vec.begin(), (vec.end() - Unit));
+    for (const auto& element : vec)
+        std::cout << element << " ";
+    return 0;
+}
